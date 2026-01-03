c99b1c7948167a6cabd6bda0c2745b8d00165563
Hugo Deleon <hugodeleon370@gmail.com>
2026-01-03 12:43:49 -0300

    Update bd_sqlite.py
    Conexão com SQLite
    Criação automática de tabela
    Insert
    Select
    Delete

--- bd_sqlite.py
+++ bd_sqlite.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-DB_NAME = "tarefas.db"
+SQLITE_DB = "tarefas.db"
 
 def conectar():
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(SQLITE_DB)
     return conn
 
 
@@ -13,12 +13,44 @@ def criar_tabela():
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS tarefas (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            titulo TEXT NOT NULL,
-            descricao TEXT NOT NULL,
-            status TEXT NOT NULL
+            tar_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tar_tit TEXT NOT NULL,
+            tar_desc TEXT NOT NULL,
+            tar_stt TEXT NOT NULL
         )
     """)
+
+    conn.commit()
+    conn.close()
+
+
+def inserir_tarefa(tar_tit, tar_desc, tar_stt):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO tarefas (tar_tit, tar_desc, tar_stt) VALUES(?, ?, ?)""", (tar_tit, tar_desc, tar_stt))
+
+    conn.commit()
+    conn.close()
+
+
+def listar_tarefas():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""SELECT tar_id, tar_tit, tar_desc, tar_stt FROM tarefas""")
+
+    tarefas = cursor.fetchall()
+    conn.close()
+
+    return tarefas
+
+def exluir_tarefa(tar_id):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM tarefas WHERE tar_id = ?", (tar_id,))
 
     conn.commit()
     conn.close()
